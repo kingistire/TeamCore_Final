30845f320b42d6021b611d11fe7f12447e4045f1
Jason Tam <jasontam2233@gmail.com>
2018-08-22 21:26:33 +1000

    Added template (incomplete)

--- Login/Login/Interview.Designer.cs
+++ Login/Login/Interview.Designer.cs
@@ -23,33 +23,303 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.label1 = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Interview));
+            this.interviewPanel = new System.Windows.Forms.Panel();
+            this.bottomRightALotBtn = new System.Windows.Forms.Button();
+            this.bottomRightALittleBtn = new System.Windows.Forms.Button();
+            this.bottomMidALotBtn = new System.Windows.Forms.Button();
+            this.bottomMidALittleBtn = new System.Windows.Forms.Button();
+            this.bottomLeftALotBtn = new System.Windows.Forms.Button();
+            this.bottomLeftALittleBtn = new System.Windows.Forms.Button();
+            this.topRightALotBtn = new System.Windows.Forms.Button();
+            this.topRightALittleBtn = new System.Windows.Forms.Button();
+            this.topMidALotBtn = new System.Windows.Forms.Button();
+            this.topMidALittleBtn = new System.Windows.Forms.Button();
+            this.topRightPBALotBtn = new System.Windows.Forms.Button();
+            this.topLeftPBALittleBtn = new System.Windows.Forms.Button();
+            this.bottomRightPB = new System.Windows.Forms.PictureBox();
+            this.bottomMidPB = new System.Windows.Forms.PictureBox();
+            this.bottomLeftPB = new System.Windows.Forms.PictureBox();
+            this.topRightPB = new System.Windows.Forms.PictureBox();
+            this.topMidPB = new System.Windows.Forms.PictureBox();
+            this.topLeftPB = new System.Windows.Forms.PictureBox();
+            this.nextInterviewSlideBTN = new System.Windows.Forms.Button();
+            this.previousInterviewSlideBtn = new System.Windows.Forms.Button();
+            this.interviewPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bottomRightPB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bottomMidPB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bottomLeftPB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.topRightPB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.topMidPB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.topLeftPB)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // interviewPanel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(92, 103);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(96, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "great success";
+            this.interviewPanel.Controls.Add(this.previousInterviewSlideBtn);
+            this.interviewPanel.Controls.Add(this.nextInterviewSlideBTN);
+            this.interviewPanel.Controls.Add(this.bottomRightALotBtn);
+            this.interviewPanel.Controls.Add(this.bottomRightALittleBtn);
+            this.interviewPanel.Controls.Add(this.bottomMidALotBtn);
+            this.interviewPanel.Controls.Add(this.bottomMidALittleBtn);
+            this.interviewPanel.Controls.Add(this.bottomLeftALotBtn);
+            this.interviewPanel.Controls.Add(this.bottomLeftALittleBtn);
+            this.interviewPanel.Controls.Add(this.topRightALotBtn);
+            this.interviewPanel.Controls.Add(this.topRightALittleBtn);
+            this.interviewPanel.Controls.Add(this.topMidALotBtn);
+            this.interviewPanel.Controls.Add(this.topMidALittleBtn);
+            this.interviewPanel.Controls.Add(this.topRightPBALotBtn);
+            this.interviewPanel.Controls.Add(this.topLeftPBALittleBtn);
+            this.interviewPanel.Controls.Add(this.bottomRightPB);
+            this.interviewPanel.Controls.Add(this.bottomMidPB);
+            this.interviewPanel.Controls.Add(this.bottomLeftPB);
+            this.interviewPanel.Controls.Add(this.topRightPB);
+            this.interviewPanel.Controls.Add(this.topMidPB);
+            this.interviewPanel.Controls.Add(this.topLeftPB);
+            this.interviewPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.interviewPanel.Location = new System.Drawing.Point(0, 0);
+            this.interviewPanel.Name = "interviewPanel";
+            this.interviewPanel.Size = new System.Drawing.Size(986, 527);
+            this.interviewPanel.TabIndex = 0;
             // 
-            // Form1
+            // bottomRightALotBtn
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.bottomRightALotBtn.Location = new System.Drawing.Point(809, 470);
+            this.bottomRightALotBtn.Name = "bottomRightALotBtn";
+            this.bottomRightALotBtn.Size = new System.Drawing.Size(75, 23);
+            this.bottomRightALotBtn.TabIndex = 17;
+            this.bottomRightALotBtn.Text = "A Lot";
+            this.bottomRightALotBtn.UseVisualStyleBackColor = true;
+            this.bottomRightALotBtn.Click += new System.EventHandler(this.bottomRightALotBtn_Click);
+            // 
+            // bottomRightALittleBtn
+            // 
+            this.bottomRightALittleBtn.Location = new System.Drawing.Point(723, 470);
+            this.bottomRightALittleBtn.Name = "bottomRightALittleBtn";
+            this.bottomRightALittleBtn.Size = new System.Drawing.Size(75, 23);
+            this.bottomRightALittleBtn.TabIndex = 16;
+            this.bottomRightALittleBtn.Text = "A Little";
+            this.bottomRightALittleBtn.UseVisualStyleBackColor = true;
+            this.bottomRightALittleBtn.Click += new System.EventHandler(this.bottomRightALittleBtn_Click);
+            // 
+            // bottomMidALotBtn
+            // 
+            this.bottomMidALotBtn.Location = new System.Drawing.Point(473, 470);
+            this.bottomMidALotBtn.Name = "bottomMidALotBtn";
+            this.bottomMidALotBtn.Size = new System.Drawing.Size(75, 23);
+            this.bottomMidALotBtn.TabIndex = 15;
+            this.bottomMidALotBtn.Text = "A Lot";
+            this.bottomMidALotBtn.UseVisualStyleBackColor = true;
+            this.bottomMidALotBtn.Click += new System.EventHandler(this.bottomMidALotBtn_Click);
+            // 
+            // bottomMidALittleBtn
+            // 
+            this.bottomMidALittleBtn.Location = new System.Drawing.Point(387, 470);
+            this.bottomMidALittleBtn.Name = "bottomMidALittleBtn";
+            this.bottomMidALittleBtn.Size = new System.Drawing.Size(75, 23);
+            this.bottomMidALittleBtn.TabIndex = 14;
+            this.bottomMidALittleBtn.Text = "A Little";
+            this.bottomMidALittleBtn.UseVisualStyleBackColor = true;
+            this.bottomMidALittleBtn.Click += new System.EventHandler(this.bottomMidALittleBtn_Click);
+            // 
+            // bottomLeftALotBtn
+            // 
+            this.bottomLeftALotBtn.Location = new System.Drawing.Point(156, 470);
+            this.bottomLeftALotBtn.Name = "bottomLeftALotBtn";
+            this.bottomLeftALotBtn.Size = new System.Drawing.Size(75, 23);
+            this.bottomLeftALotBtn.TabIndex = 13;
+            this.bottomLeftALotBtn.Text = "A Lot";
+            this.bottomLeftALotBtn.UseVisualStyleBackColor = true;
+            this.bottomLeftALotBtn.Click += new System.EventHandler(this.bottomLeftALotBtn_Click);
+            // 
+            // bottomLeftALittleBtn
+            // 
+            this.bottomLeftALittleBtn.Location = new System.Drawing.Point(70, 470);
+            this.bottomLeftALittleBtn.Name = "bottomLeftALittleBtn";
+            this.bottomLeftALittleBtn.Size = new System.Drawing.Size(75, 23);
+            this.bottomLeftALittleBtn.TabIndex = 12;
+            this.bottomLeftALittleBtn.Text = "A Little";
+            this.bottomLeftALittleBtn.UseVisualStyleBackColor = true;
+            this.bottomLeftALittleBtn.Click += new System.EventHandler(this.bottomLeftALittleBtn_Click);
+            // 
+            // topRightALotBtn
+            // 
+            this.topRightALotBtn.Location = new System.Drawing.Point(809, 204);
+            this.topRightALotBtn.Name = "topRightALotBtn";
+            this.topRightALotBtn.Size = new System.Drawing.Size(75, 23);
+            this.topRightALotBtn.TabIndex = 11;
+            this.topRightALotBtn.Text = "A Lot";
+            this.topRightALotBtn.UseVisualStyleBackColor = true;
+            this.topRightALotBtn.Click += new System.EventHandler(this.topRightALotBtn_Click);
+            // 
+            // topRightALittleBtn
+            // 
+            this.topRightALittleBtn.Location = new System.Drawing.Point(723, 204);
+            this.topRightALittleBtn.Name = "topRightALittleBtn";
+            this.topRightALittleBtn.Size = new System.Drawing.Size(75, 23);
+            this.topRightALittleBtn.TabIndex = 10;
+            this.topRightALittleBtn.Text = "A Little";
+            this.topRightALittleBtn.UseVisualStyleBackColor = true;
+            this.topRightALittleBtn.Click += new System.EventHandler(this.topRightALittleBtn_Click);
+            // 
+            // topMidALotBtn
+            // 
+            this.topMidALotBtn.Location = new System.Drawing.Point(473, 204);
+            this.topMidALotBtn.Name = "topMidALotBtn";
+            this.topMidALotBtn.Size = new System.Drawing.Size(75, 23);
+            this.topMidALotBtn.TabIndex = 9;
+            this.topMidALotBtn.Text = "A Lot";
+            this.topMidALotBtn.UseVisualStyleBackColor = true;
+            this.topMidALotBtn.Click += new System.EventHandler(this.topMidALotBtn_Click);
+            // 
+            // topMidALittleBtn
+            // 
+            this.topMidALittleBtn.Location = new System.Drawing.Point(387, 204);
+            this.topMidALittleBtn.Name = "topMidALittleBtn";
+            this.topMidALittleBtn.Size = new System.Drawing.Size(75, 23);
+            this.topMidALittleBtn.TabIndex = 8;
+            this.topMidALittleBtn.Text = "A Little";
+            this.topMidALittleBtn.UseVisualStyleBackColor = true;
+            this.topMidALittleBtn.Click += new System.EventHandler(this.topMidALittleBtn_Click);
+            // 
+            // topRightPBALotBtn
+            // 
+            this.topRightPBALotBtn.Location = new System.Drawing.Point(156, 204);
+            this.topRightPBALotBtn.Name = "topRightPBALotBtn";
+            this.topRightPBALotBtn.Size = new System.Drawing.Size(75, 23);
+            this.topRightPBALotBtn.TabIndex = 7;
+            this.topRightPBALotBtn.Text = "A Lot";
+            this.topRightPBALotBtn.UseVisualStyleBackColor = true;
+            this.topRightPBALotBtn.Click += new System.EventHandler(this.topRightPBALotBtn_Click);
+            // 
+            // topLeftPBALittleBtn
+            // 
+            this.topLeftPBALittleBtn.Location = new System.Drawing.Point(70, 204);
+            this.topLeftPBALittleBtn.Name = "topLeftPBALittleBtn";
+            this.topLeftPBALittleBtn.Size = new System.Drawing.Size(75, 23);
+            this.topLeftPBALittleBtn.TabIndex = 6;
+            this.topLeftPBALittleBtn.Text = "A Little";
+            this.topLeftPBALittleBtn.UseVisualStyleBackColor = true;
+            this.topLeftPBALittleBtn.Click += new System.EventHandler(this.topLeftPBALittleBtn_Click);
+            // 
+            // bottomRightPB
+            // 
+            this.bottomRightPB.Location = new System.Drawing.Point(723, 304);
+            this.bottomRightPB.Name = "bottomRightPB";
+            this.bottomRightPB.Size = new System.Drawing.Size(161, 151);
+            this.bottomRightPB.TabIndex = 5;
+            this.bottomRightPB.TabStop = false;
+            this.bottomRightPB.Paint += new System.Windows.Forms.PaintEventHandler(this.bottomRightPB_Paint);
+            // 
+            // bottomMidPB
+            // 
+            this.bottomMidPB.Location = new System.Drawing.Point(387, 304);
+            this.bottomMidPB.Name = "bottomMidPB";
+            this.bottomMidPB.Size = new System.Drawing.Size(161, 151);
+            this.bottomMidPB.TabIndex = 4;
+            this.bottomMidPB.TabStop = false;
+            this.bottomMidPB.Paint += new System.Windows.Forms.PaintEventHandler(this.bottomMidPB_Paint);
+            // 
+            // bottomLeftPB
+            // 
+            this.bottomLeftPB.Location = new System.Drawing.Point(70, 304);
+            this.bottomLeftPB.Name = "bottomLeftPB";
+            this.bottomLeftPB.Size = new System.Drawing.Size(161, 151);
+            this.bottomLeftPB.TabIndex = 3;
+            this.bottomLeftPB.TabStop = false;
+            this.bottomLeftPB.Paint += new System.Windows.Forms.PaintEventHandler(this.bottomLeftPB_Paint);
+            // 
+            // topRightPB
+            // 
+            this.topRightPB.Location = new System.Drawing.Point(723, 37);
+            this.topRightPB.Name = "topRightPB";
+            this.topRightPB.Size = new System.Drawing.Size(161, 151);
+            this.topRightPB.TabIndex = 2;
+            this.topRightPB.TabStop = false;
+            this.topRightPB.Paint += new System.Windows.Forms.PaintEventHandler(this.topRightPB_Paint);
+            // 
+            // topMidPB
+            // 
+            this.topMidPB.Location = new System.Drawing.Point(387, 37);
+            this.topMidPB.Name = "topMidPB";
+            this.topMidPB.Size = new System.Drawing.Size(161, 151);
+            this.topMidPB.TabIndex = 1;
+            this.topMidPB.TabStop = false;
+            this.topMidPB.Paint += new System.Windows.Forms.PaintEventHandler(this.topMidPB_Paint);
+            // 
+            // topLeftPB
+            // 
+            this.topLeftPB.Image = ((System.Drawing.Image)(resources.GetObject("topLeftPB.Image")));
+            this.topLeftPB.Location = new System.Drawing.Point(70, 37);
+            this.topLeftPB.Name = "topLeftPB";
+            this.topLeftPB.Size = new System.Drawing.Size(161, 151);
+            this.topLeftPB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.topLeftPB.TabIndex = 0;
+            this.topLeftPB.TabStop = false;
+            this.topLeftPB.Paint += new System.Windows.Forms.PaintEventHandler(this.topLeftPB_Paint);
+            // 
+            // nextInterviewSlideBTN
+            // 
+            this.nextInterviewSlideBTN.Location = new System.Drawing.Point(899, 251);
+            this.nextInterviewSlideBTN.Name = "nextInterviewSlideBTN";
+            this.nextInterviewSlideBTN.Size = new System.Drawing.Size(75, 23);
+            this.nextInterviewSlideBTN.TabIndex = 18;
+            this.nextInterviewSlideBTN.Text = "Next";
+            this.nextInterviewSlideBTN.UseVisualStyleBackColor = true;
+            this.nextInterviewSlideBTN.Click += new System.EventHandler(this.nextInterviewSlideBTN_Click);
+            // 
+            // previousInterviewSlideBtn
+            // 
+            this.previousInterviewSlideBtn.Location = new System.Drawing.Point(13, 251);
+            this.previousInterviewSlideBtn.Name = "previousInterviewSlideBtn";
+            this.previousInterviewSlideBtn.Size = new System.Drawing.Size(75, 23);
+            this.previousInterviewSlideBtn.TabIndex = 19;
+            this.previousInterviewSlideBtn.Text = "Previous";
+            this.previousInterviewSlideBtn.UseVisualStyleBackColor = true;
+            this.previousInterviewSlideBtn.Visible = false;
+            // 
+            // Interview
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
-            this.Controls.Add(this.label1);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(986, 527);
+            this.Controls.Add(this.interviewPanel);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Interview";
             this.Text = "Form1";
+            this.interviewPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.bottomRightPB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bottomMidPB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bottomLeftPB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.topRightPB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.topMidPB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.topLeftPB)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel interviewPanel;
+        private System.Windows.Forms.PictureBox bottomRightPB;
+        private System.Windows.Forms.PictureBox bottomMidPB;
+        private System.Windows.Forms.PictureBox bottomLeftPB;
+        private System.Windows.Forms.PictureBox topRightPB;
+        private System.Windows.Forms.PictureBox topMidPB;
+        private System.Windows.Forms.PictureBox topLeftPB;
+        private System.Windows.Forms.Button topRightPBALotBtn;
+        private System.Windows.Forms.Button topLeftPBALittleBtn;
+        private System.Windows.Forms.Button bottomRightALotBtn;
+        private System.Windows.Forms.Button bottomRightALittleBtn;
+        private System.Windows.Forms.Button bottomMidALotBtn;
+        private System.Windows.Forms.Button bottomMidALittleBtn;
+        private System.Windows.Forms.Button bottomLeftALotBtn;
+        private System.Windows.Forms.Button bottomLeftALittleBtn;
+        private System.Windows.Forms.Button topRightALotBtn;
+        private System.Windows.Forms.Button topRightALittleBtn;
+        private System.Windows.Forms.Button topMidALotBtn;
+        private System.Windows.Forms.Button topMidALittleBtn;
+        private System.Windows.Forms.Button previousInterviewSlideBtn;
+        private System.Windows.Forms.Button nextInterviewSlideBTN;
     }
 }
--- Login/Login/Interview.cs
+++ Login/Login/Interview.cs
@@ -12,6 +12,232 @@ namespace Login {
     public partial class Interview : Form {
         public Interview() {
             InitializeComponent();
+            //Dynamically create the circular picture boxes
+            createCirclePB(bottomLeftPB);
+            createCirclePB(bottomRightPB);
+            createCirclePB(bottomMidPB);
+            createCirclePB(topLeftPB);
+            createCirclePB(topRightPB);
+            createCirclePB(topMidPB);
+        }
+
+        private Pen aLittle = new Pen(Color.Blue, 5);
+        private Pen aLot = new Pen(Color.DarkBlue, 10);
+        private bool aLittleBool = false;
+        private bool aLotBool = false;
+        private bool isLittleSelected = false;
+        private bool isLotSelected = false;
+
+        private bool nextBtnClick = false;
+        private int nextCounter = 0;
+        private int interview2Counter = 0;
+
+        /// <summary>
+        /// Dynamically create circular pictureboxes
+        /// </summary>
+        /// <param name="pictureBox">picturebox name</param>
+        private void createCirclePB(PictureBox pictureBox) {
+            System.Drawing.Drawing2D.GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath();
+            path.AddEllipse(0, 0, pictureBox.Width, pictureBox.Height);
+            pictureBox.Region = new Region(path);
+        }
+
+        /// <summary>
+        /// Dynamically change picturebox image
+        /// </summary>
+        /// <param name="pictureBox">Name of PictureBox</param>
+        /// <param name="imageURL">Path to the image as a string</param>
+        private void changePictureBoxImage(PictureBox pictureBox, string imageURL, int numberOfSlidesForInterview) {
+            //pictureBox.Image = new Bitmap (@imageURL);
+
+            //Change Image
+            if (nextBtnClick && nextCounter == 1) {
+                pictureBox.Image = new Bitmap(@imageURL);
+            } else if (nextCounter == 2) {
+                pictureBox.Image = new Bitmap(@imageURL);
+            } else if (nextCounter == numberOfSlidesForInterview) {
+                if (MessageBox.Show("You have completed the visual interview. The next interview is XXXX.", "Ok", MessageBoxButtons.OK) == DialogResult.OK) {
+                    pictureBox.Image = new Bitmap(@imageURL);
+                    nextCounter = 0;
+                    nextInterviewSlideBTN.Visible = false;
+                    nextInterviewSlideBTN.Enabled = false;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Draws the circle border on selected picturebox
+        /// </summary>
+        /// <param name="pictureBoxName">Picturebox name</param>
+        /// <param name="e"></param>
+        private void DeterminePenSizeDrawing(PictureBox pictureBoxName, PaintEventArgs e) {
+          //If aLittle Button is clicked
+            if (aLittleBool) {
+                DrawCircle(aLittle, e, 0, 0, pictureBoxName.Size.Width, pictureBoxName.Size.Height);
+                Console.WriteLine("A Little Button was push" + "\n");
+                //If aLot button is clicked
+            } else if (aLotBool) {
+                DrawCircle(aLot, e, 0, 0, pictureBoxName.Size.Width, pictureBoxName.Size.Height);
+                Console.WriteLine("A Lot Button was push" + "\n");
+            }
+
+        }
+
+        /// <summary>
+        /// Draw a circle
+        /// </summary>
+        /// <param name="pen"></param>
+        /// <param name="pictureBox"></param>
+        /// <param name="e"></param>
+        /// <param name="x">Boundary of the x value</param>
+        /// <param name="y">Boundary of the y value</param>
+        /// <param name="width">Must be an int of the picture box i.e. nameOfPictureBox.Size.Width</param>
+        /// <param name="height">Same with height but with nameOfPictureBox.Size.Height</param>
+        private void DrawCircle(Pen pen, PaintEventArgs e, int x, int y, int width, int height) {
+            Graphics g = e.Graphics;
+            g.DrawEllipse(pen, x, y, width, height);
+        }
+
+        //Code from here will update the pictureboxes when the user has clicked on a button
+        private void topLeftPB_Paint(object sender, PaintEventArgs e) {
+            DeterminePenSizeDrawing(topLeftPB, e);
+
+        }
+
+        private void topMidPB_Paint(object sender, PaintEventArgs e) {
+            DeterminePenSizeDrawing(topMidPB, e);
+        }
+
+        private void topRightPB_Paint(object sender, PaintEventArgs e) {
+            DeterminePenSizeDrawing(topRightPB, e);
+        }
+
+        private void bottomLeftPB_Paint(object sender, PaintEventArgs e) {
+            DeterminePenSizeDrawing(bottomLeftPB, e);
+        }
+
+        private void bottomMidPB_Paint(object sender, PaintEventArgs e) {
+            DeterminePenSizeDrawing(bottomMidPB, e);
+        }
+
+        private void bottomRightPB_Paint(object sender, PaintEventArgs e) {
+            DeterminePenSizeDrawing(bottomRightPB, e);
+        }
+
+        /// <summary>
+        /// Call this function when user is going to click on A Little
+        /// </summary>
+        private void changeBoolALittle() {
+            aLittleBool = true;
+            aLotBool = false;
+            isLittleSelected = true;
+            isLotSelected = false;
+        }
+        /// <summary>
+        /// Call this function when user is going to click A Lot
+        /// </summary>
+        private void changeBoolALot() {
+            aLittleBool = false;
+            aLotBool = true;
+            isLittleSelected = false;
+            isLotSelected = true;
+        }
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////// BUTTON CLICKS FOR A LITT AND A LOT ////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        private void topRightPBALotBtn_Click(object sender, EventArgs e) {
+            changeBoolALot();
+            //This will refresh the picturebox
+            topLeftPB.Invalidate();
+        }
+
+        private void topLeftPBALittleBtn_Click(object sender, EventArgs e) {
+            changeBoolALittle();
+            //This will refresh the picture box
+            topLeftPB.Invalidate();
+        }
+
+        private void topMidALittleBtn_Click(object sender, EventArgs e) {
+            changeBoolALittle();
+            //This will refresh the picture box
+            topMidPB.Invalidate();
+        }
+
+        private void topMidALotBtn_Click(object sender, EventArgs e) {
+            changeBoolALot();
+            //This will refresh the picture box
+            topMidPB.Invalidate();
+        }
+
+        private void topRightALittleBtn_Click(object sender, EventArgs e) {
+            changeBoolALittle();
+            //This will refresh the picture box
+            topRightPB.Invalidate();
+        }
+
+        private void topRightALotBtn_Click(object sender, EventArgs e) {
+            changeBoolALot();
+            //This will refresh the picture box
+            topRightPB.Invalidate();
+        }
+
+        private void bottomLeftALittleBtn_Click(object sender, EventArgs e) {
+            changeBoolALittle();
+            //This will refresh the picture box
+            bottomLeftPB.Invalidate();
+        }
+
+        private void bottomLeftALotBtn_Click(object sender, EventArgs e) {
+            changeBoolALot();
+            //This will refresh the picture box
+            bottomLeftPB.Invalidate();
+        }
+
+        private void bottomMidALittleBtn_Click(object sender, EventArgs e) {
+            changeBoolALittle();
+            //This will refresh the picture box
+            bottomMidPB.Invalidate();
+        }
+
+        private void bottomMidALotBtn_Click(object sender, EventArgs e) {
+            changeBoolALot();
+            //This will refresh the picture box
+            bottomMidPB.Invalidate();
+        }
+
+        private void bottomRightALittleBtn_Click(object sender, EventArgs e) {
+            changeBoolALittle();
+            //This will refresh the picture box
+            bottomRightPB.Invalidate();
+        }
+
+        private void bottomRightALotBtn_Click(object sender, EventArgs e) {
+            changeBoolALot();
+            //This will refresh the picture box
+            bottomRightPB.Invalidate();
+        }
+
+        /// <summary>
+        /// Change a little and a lot to false when the next button is pressed, effectively erasing the drawings.
+        /// Increase next counter to replace image
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void nextInterviewSlideBTN_Click(object sender, EventArgs e) {
+            changeButtonBoolsOnNext();
+            nextCounter++;
+            //Call changePictureBoxImage method here
+
+        }
+
+        /// <summary>
+        /// Function to determine if the next button has been clicked, and change the a Little and a Lot button to false to erase the drawing
+        /// </summary>
+        private void changeButtonBoolsOnNext() {
+            nextBtnClick = true;
+            aLittleBool = false;
+            aLotBool = false;
         }
     }
 }
